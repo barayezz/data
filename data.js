3642a516c064662ddabc779ccb50406f396d0de4
barayezz <31810685+barayezz@users.noreply.github.com>
2017-09-10 03:25:50 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,143 @@
+var allowedUrls = {
+    '5gAWcpdd': {
+     title: 'الولد المنحرف والام النائمة',
+     watching: 'https://openload.co/embed/IprnZTppyFg/',
+     l1: 'http://linkshrink.net/7C3ulJ'
+    },
+    '7j2fAKqD': {
+     title: 'الولد المنحرف والفخاذ المثيرة',
+     watching: 'https://openload.co/embed/aF3beJIYgos/',
+     l1: 'http://linkshrink.net/7rFoKd'
+    },
+    'sU9CX4RB': {
+     title: 'الغسيل المتسخ',
+     watching: 'https://openload.co/embed/Zb3HcHQD-PE/',
+     l1: 'http://linkshrink.net/7jJpMg'
+    },
+    'Pdu25abA': {
+     title: 'زوجة الاب تجلس علي رجل ابنها اثناء المباراة',
+     watching: 'https://openload.co/embed/SpT5NQgTjwA/',
+     l1: 'http://linkshrink.net/7lFXtd'
+    },
+    'Xj2Pcq6Z': {
+     title: 'عاملة الفندق الشقراء المربربة الجميلة',
+     watching: 'https://openload.co/embed/iuKM7NOJR3E/',
+     l1: 'http://linkshrink.net/7Pn4f1'
+    },
+    '2GfhcBS6': {
+     title: 'داخل غرف المساچ الحقيقي مترجم',
+     watching: 'https://openload.co/embed/jeJ8X7hkEho/',
+     l1: 'http://linkshrink.net/7ha340'
+    },
+    'QURAZ5cv': {
+     title: 'معلمة الانجليزية وإغراء المال',
+     watching: 'https://openload.co/embed/It3FekOWWDY/',
+     l1: 'http://linkshrink.net/7aG3D1'
+    },
+    'ksSSQL7a': {
+     title: 'طالبة الطب وإغراء المال',
+     watching: 'https://openload.co/embed/IJy-WchuNEc/',
+     l1: 'http://linkshrink.net/7N4HbO'
+    },
+    'F7mY8Rz4': {
+     title: 'الزوجة الخائنة والزوج المغفل',
+     watching: 'https://openload.co/embed/jrAFGjWJUA0/',
+     l1: 'http://linkshrink.net/7Tuyjg'
+    },
+    'mX7VYBtJ': {
+     title: 'الطبيب المزيف',
+     watching: 'https://openload.co/embed/tH9oWJpO7Xs/',
+     l1: 'http://linkshrink.net/7OEVL6'
+    },
+    'Ks996uGY': {
+     title: 'المساچ المترجم 2',
+     watching: 'https://openload.co/embed/4e1g54M-3NM/',
+     l1: 'http://linkshrink.net/79AtdP'
+    },
+    'AM96Bca5': {
+     title: 'سحاق مقابل الوظيفة',
+     watching: 'https://openload.co/embed/ZkNYdJHjQ9c/',
+     l1: 'http://linkshrink.net/7vuYyy'
+    },
+    'u6t4USEH': {
+     title: 'دكتور چوني والكشف خلف الملاءة',
+     watching: 'https://openload.co/embed/oStQVsBs_KY/',
+     l1: 'http://linkshrink.net/7ZHe2k'
+    },
+    'EaYF7Z6J': {
+     title: 'الشقراء الروسية وإغراء المال',
+     watching: 'https://openload.co/embed/wEfq5eU0yiI/',
+     l1: 'http://linkshrink.net/7gQpW0'
+    },
+    '6LB9RFrK': {
+     title: 'الاخ المتجسس والاخت الكبرى',
+     watching: 'https://openload.co/embed/pp_G4GuNm0Q/',
+     l1: 'http://linkshrink.net/7ePvL9'
+    },
+    '7snvjXmU': {
+     title: 'المساچ الحقيقي الجزء الثالث',
+     watching: 'https://openload.co/embed/ysFSfGBgS-M/',
+     l1: 'http://linkshrink.net/7LzMHl'
+    },
+    'T3wXTtJg': {
+     title: 'دكتور جوني يعالج البزاز',
+     l1: 'http://linkshrink.net/7PlLvg',
+     l2: 'http://www.adyou.me/huQr'
+    },
+    'aDh4DECw': {
+     title: 'اختي لاتمانع بالمشاركة',
+     l1: 'http://linkshrink.net/7chVnz',
+     l2: 'http://www.adyou.me/iYcJ'
+    },
+    '3kpnQHkS': {
+     title: 'چوني و وصيفة العروس ج1',
+     l1: 'http://linkshrink.net/7LrLDE',
+     l2: 'http://www.adyou.me/vCEG'
+    },
+    '9kagFhPL': {
+     title: 'چوني و ام العروس ج2',
+     l1: 'http://linkshrink.net/7fw4l4',
+     l2: 'http://www.adyou.me/y16A'
+    },
+    'fqrt6CLu': {
+     title: 'النيك مقابل المشاركة بالمسابقة',
+     watching: 'https://openload.co/embed/OWSNKeEYKFs/',
+     l1: 'http://linkshrink.net/7C1j6N'
+    },
+    'GMAFxf3p': {
+     title: 'الطبيب المزيف - شفرات الكس الكبيرة',
+     watching: 'https://openload.co/embed/O7rat21Tlh4/',
+     l1: 'http://linkshrink.net/7rSndz'
+    },
+    'AaJT7qgS': {
+     title: 'سهرة حمرة مع فتاة يابانية',
+     watching: 'https://openload.co/embed/LClGwOZHdWU/',
+     l1: 'http://linkshrink.net/7H5oCr'
+    },
+    'HcG2ULuq': {
+     title: 'النيك مقابل الايباد',
+     watching: 'https://openload.co/embed/Csg_GzqIQO8/',
+     l1: 'http://linkshrink.net/7KMsea',
+     l2: 'http://www.adyou.co/0v8s'
+    }, '9Q5HtfZn': {
+     title: 'النيك مقابل النقود بحمام الفندق الرجالي',
+     watching: 'https://openload.co/embed/sY9ANsQWVXg/',
+     l1: 'http://linkshrink.net/7IvMMT',
+     l2: 'http://www.adyou.co/Il4F'
+    }, 'TFejTg5P': {
+     title: 'النيك مقابل محفظة النقود',
+     watching: 'https://openload.co/embed/2MLn7yNW8YE/',
+     l1: 'http://linkshrink.net/7wXsoP',
+     l2: 'http://www.adyou.co/UjqW'
+    }, '5SXj9EJr': {
+     title: 'السقوط من علي الدراجة والعلاج بالنيك',
+     watching: 'https://openload.co/embed/fDp35A24PE4/',
+     l1: 'http://linkshrink.net/7qelJK',
+     l2: 'http://www.adyou.co/XKe2'
+    }, 'a5g9bTCY': {
+     title: 'طياز الاخت الكبيرة والهاتف المفقود',
+     watching: 'https://openload.co/embed/KzoYGhutFY0/',
+     l1: 'http://linkshrink.net/7oSOnp',
+     l2: 'http://www.adyou.co/n9sN'
+    }
+}
